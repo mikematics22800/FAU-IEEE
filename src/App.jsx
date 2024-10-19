8805a2a9d94bcfda3c4536373777e30ea019fac4
Michael Medina <michaeljmedina22800@gmail.com>
2024-10-19 16:00:56 -0400

    implemented react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, About } from "./routes";
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Home/>,
+  },
+  {
+    path: "about",
+    element: <About/>,
+  },
+]);
+
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       
-    </>
+    </RouterProvider>
   )
 }
 
