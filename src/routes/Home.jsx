4bed45356b2471281148deb52f8468757c5411ad
Michael Medina <michaeljmedina22800@gmail.com>
2024-10-24 17:15:29 -0400

    added horizontal padding to footer

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
           <h1>Welcome Fellow Engineers!</h1>
           <p>
             This club will help you develop your professional engineering skills to land a great internship and a dream job in hardware, software, and robotics.
-            You will learn invaluable skills including printed circuit board (PCB) design, Altium, soldering, digital multimeter, power supply, programming, troubleshooting, oscilloscope, 3D printing, computer-aided design (CAD), and simulation.
+            Here, you will learn invaluable skills including printed circuit board (PCB) design, Altium, soldering, digital multimeter, power supply, programming, troubleshooting, oscilloscope, 3D printing, computer-aided design (CAD), and simulation.
             The Institute of Electronics and Electrical Engineering is a professional association headquartered in New York City, dedicated to advancing technological innovation and excellence.
             Design Electronics and join the team that makes it happen!
           </p>
